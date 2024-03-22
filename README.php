d4023686482844ff3e0c307564809d7e71d9bdee
Diego-m-dev <148476328+Diego-m-dev@users.noreply.github.com>
2024-03-22 13:59:10 +0000

    1

--- README.php
+++ README.php
@@ -9,7 +9,7 @@ include_once $_SERVER['DOCUMENT_ROOT'] . '/Suas-Tech/cadunico/controller/acesso_
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/style-menu.css">
+    <link rel="stylesheet" href="style-menu.css">
     <link rel="website icon" type="png" href="../img/logo.png">
     <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
     <title>TechSUAS - Cras Santo Afonso</title>
